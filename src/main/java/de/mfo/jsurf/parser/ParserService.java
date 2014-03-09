f29db60474562e7daf666b758dd95813a82428b3
ferpe <ferpe.email@gmail.com>
2014-03-09 19:44:05 -0300

    parser service class

---
+++ src/main/java/de/mfo/jsurf/parser/ParserService.java
@@ -0,0 +1,11 @@
+package de.mfo.jsurf.parser;
+
+import de.mfo.jsurf.algebra.PolynomialOperation;
+
+public class ParserService
+{
+    public static PolynomialOperation parse(String s) throws Exception
+    {
+	return AlgebraicExpressionParser.parse(s);
+    }
+}
