6ce3bbed3711144e12871f952b704ccd9eafac97
ferpe <ferpe.email@gmail.com>
2014-03-16 20:48:58 -0300

    automatic git merge fixed

--- src/main/java/de/mfo/jsurf/Main.java
+++ src/main/java/de/mfo/jsurf/Main.java
@@ -34,134 +34,89 @@ import de.mfo.jsurf.util.FileFormat;
 
 public class Main
 {
-<<<<<<< HEAD
-=======
-
->>>>>>> ae21e7714c6a385ce00e4031b769abb6e9869b67
-    static int size = 100;
+    static int size= 100;
     static AntiAliasingMode aam;
     static AntiAliasingPattern aap;
 
-    static CPUAlgebraicSurfaceRenderer asr = new CPUAlgebraicSurfaceRenderer();
+    static CPUAlgebraicSurfaceRenderer asr= new CPUAlgebraicSurfaceRenderer();
     protected static String output_filename;
     protected static int quality;
 
-<<<<<<< HEAD
     /**
      * @param args
      */
     public static void main(String[] args) throws Exception
     {
-	Properties jsurf = new Properties();
-	String jsurf_filename = readCommandLine(args, jsurf);
+	Properties jsurf= new Properties();
+	String jsurf_filename= readCommandLine(args, jsurf);
 
 	FileFormat.load(jsurf, asr);
 
 	asr.setAntiAliasingMode(aam);
 	asr.setAntiAliasingPattern(aap);
 
-	ImgBuffer ib = new ImgBuffer(size, size);
+	ImgBuffer ib= new ImgBuffer(size, size);
 	asr.draw(ib.rgbBuffer, size, size);
-
-	BufferedImageGenerator imageGenerator = new BufferedImageGenerator(size, jsurf_filename, output_filename);
+	
+	BufferedImageGenerator imageGenerator= new BufferedImageGenerator(size, jsurf_filename, output_filename);
 	imageGenerator.draw(ib, size);
     }
 
     private static String readCommandLine(String[] args, Properties jsurf)
     {
-	String jsurf_filename = "";
-	output_filename = null;
-=======
-    static BufferedImage createBufferedImageFromRGB(ImgBuffer ib)
-    {
-	int w = ib.width;
-	int h = ib.height;
+	String jsurf_filename= "";
+	output_filename= null;
 
-	DirectColorModel colormodel = new DirectColorModel(24, 0xff0000, 0xff00, 0xff);
-	SampleModel sampleModel = colormodel.createCompatibleSampleModel(w, h);
-	DataBufferInt data = new DataBufferInt(ib.rgbBuffer, w * h);
-	WritableRaster raster = WritableRaster.createWritableRaster(sampleModel, data, new Point(0, 0));
-	return new BufferedImage(colormodel, raster, false, null);
-    }
-
-    public static BufferedImage flipV(BufferedImage bi)
-    {
-	AffineTransform tx = AffineTransform.getScaleInstance(1, -1);
-	tx.translate(0, -bi.getHeight(null));
-	AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-	return op.filter(bi, null);
-    }
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args)
-    {
-
-	String jsurf_filename = "";
-	String output_filename = null;
->>>>>>> ae21e7714c6a385ce00e4031b769abb6e9869b67
-
-	Options options = new Options();
+	Options options= new Options();
 
 	options.addOption("s", "size", true, "width (and height) of a image (default: " + size + ")");
 	options.addOption("q", "quality", true, "quality of the rendering: 0 (low), 1 (medium, default), 2 (high), 3 (extreme)");
 	options.addOption("o", "output", true, "output PNG into this file (- means standard output. Use ./- to denote a file literally named -.)");
 
-	CommandLineParser parser = new PosixParser();
-	HelpFormatter formatter = new HelpFormatter();
-	String cmd_line_syntax = "jsurf [options] jsurf_file";
-	String help_header = "jsurf is a renderer for algebraic surfaces. If - is specified as a filename the jsurf file is read from standard input. " + "Use ./- to denote a file literally named -.";
-	String help_footer = "";
+	CommandLineParser parser= new PosixParser();
+	HelpFormatter formatter= new HelpFormatter();
+	String cmd_line_syntax= "jsurf [options] jsurf_file";
+	String help_header= "jsurf is a renderer for algebraic surfaces. If - is specified as a filename the jsurf file is read from standard input. " + "Use ./- to denote a file literally named -.";
+	String help_footer= "";
 	try
 	{
-	    CommandLine cmd = parser.parse(options, args);
+	    CommandLine cmd= parser.parse(options, args);
 
 	    if (cmd.getArgs().length > 0)
-		jsurf_filename = cmd.getArgs()[0];
+		jsurf_filename= cmd.getArgs()[0];
 	    else
 	    {
 		formatter.printHelp(cmd_line_syntax, help_header, options, help_footer);
-<<<<<<< HEAD
 		System.exit(-1);
-=======
-		return;
->>>>>>> ae21e7714c6a385ce00e4031b769abb6e9869b67
 	    }
 
 	    if (cmd.hasOption("output"))
-		output_filename = cmd.getOptionValue("output");
+		output_filename= cmd.getOptionValue("output");
 
 	    if (cmd.hasOption("size"))
-		size = Integer.parseInt(cmd.getOptionValue("size"));
+		size= Integer.parseInt(cmd.getOptionValue("size"));
 
-<<<<<<< HEAD
-	    quality = 1;
+	    quality= 1;
 	    if (cmd.hasOption("quality"))
-		quality = Integer.parseInt(cmd.getOptionValue("quality"));
+		quality= Integer.parseInt(cmd.getOptionValue("quality"));
 
-=======
-	    int quality = 1;
-	    if (cmd.hasOption("quality"))
-		quality = Integer.parseInt(cmd.getOptionValue("quality"));
->>>>>>> ae21e7714c6a385ce00e4031b769abb6e9869b67
 	    switch (quality)
 	    {
 		case 0:
-		    aam = AntiAliasingMode.ADAPTIVE_SUPERSAMPLING;
-		    aap = AntiAliasingPattern.OG_1x1;
+		    aam= AntiAliasingMode.ADAPTIVE_SUPERSAMPLING;
+		    aap= AntiAliasingPattern.OG_1x1;
 		    break;
 		case 2:
-		    aam = AntiAliasingMode.ADAPTIVE_SUPERSAMPLING;
-		    aap = AntiAliasingPattern.OG_4x4;
+		    aam= AntiAliasingMode.ADAPTIVE_SUPERSAMPLING;
+		    aap= AntiAliasingPattern.OG_4x4;
 		    break;
 		case 3:
-		    aam = AntiAliasingMode.SUPERSAMPLING;
-		    aap = AntiAliasingPattern.OG_4x4;
+		    aam= AntiAliasingMode.SUPERSAMPLING;
+		    aap= AntiAliasingPattern.OG_4x4;
 		    break;
 		case 1:
-		    aam = AntiAliasingMode.ADAPTIVE_SUPERSAMPLING;
-		    aap = AntiAliasingPattern.QUINCUNX;
+		    aam= AntiAliasingMode.ADAPTIVE_SUPERSAMPLING;
+		    aap= AntiAliasingPattern.QUINCUNX;
 	    }
 	}
 	catch (ParseException exp)
@@ -173,7 +128,6 @@ public class Main
 	{
 	    formatter.printHelp(cmd_line_syntax, help_header, options, help_footer);
 	    System.exit(-1);
-<<<<<<< HEAD
 	}
 
 	try
@@ -191,81 +145,6 @@ public class Main
 	}
 
 	return jsurf_filename;
-=======
-	}
-
-	try
-	{
-	    Properties jsurf = new Properties();
-	    if (jsurf_filename.equals("-"))
-		jsurf.load(System.in);
-	    else
-		jsurf.load(new FileReader(jsurf_filename));
-	    FileFormat.load(jsurf, asr);
-	}
-	catch (Exception e)
-	{
-	    System.err.println("Unable to read jsurf file " + jsurf_filename);
-	    e.printStackTrace();
-	    System.exit(-2);
-	}
-
-	asr.setAntiAliasingMode(aam);
-	asr.setAntiAliasingPattern(aap);
-
-	BufferedImage bi = null;
-	try
-	{
-	    ImgBuffer ib = new ImgBuffer(size, size);
-	    asr.draw(ib.rgbBuffer, size, size);
-	    bi = flipV(createBufferedImageFromRGB(ib));
-
-	}
-	catch (Exception e)
-	{
-	    System.err.println("An error occurred during rendering.");
-	    e.printStackTrace();
-	    System.exit(-3);
-	}
-
-	if (output_filename != null)
-	{
-	    // save to file
-	    try
-	    {
-		OutputStream os;
-		if (output_filename.equals("-"))
-		    os = System.out;
-		else
-		    os = new FileOutputStream(new File(output_filename));
-		javax.imageio.ImageIO.write(bi, "png", os);
-	    }
-	    catch (Exception e)
-	    {
-		System.err.println("Unable to save to file  " + output_filename);
-		e.printStackTrace();
-		System.exit(-4);
-	    }
-	}
-	else
-	{
-	    // display the image in a window 
-	    final String window_title = "jsurf: " + jsurf_filename;
-	    final BufferedImage window_image = bi;
-	    SwingUtilities.invokeLater(new Runnable()
-	    {
-		public void run()
-		{
-		    JFrame f = new JFrame(window_title);
-		    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		    f.getContentPane().add(new JLabel(new ImageIcon(window_image)));
-		    f.pack();
-		    f.setResizable(false);
-		    f.setVisible(true);
-		}
-	    });
-	}
->>>>>>> ae21e7714c6a385ce00e4031b769abb6e9869b67
     }
 }
 
@@ -277,8 +156,8 @@ class ImgBuffer
 
     public ImgBuffer(int w, int h)
     {
-	rgbBuffer = new int[3 * w * h];
-	width = w;
-	height = h;
+	rgbBuffer= new int[3 * w * h];
+	width= w;
+	height= h;
     }
 }
--- src/main/java/de/mfo/jsurf/rendering/AlgebraicSurfaceRenderer.java
+++ src/main/java/de/mfo/jsurf/rendering/AlgebraicSurfaceRenderer.java
@@ -101,11 +101,7 @@ public abstract class AlgebraicSurfaceRenderer
 
     public void setSurfaceFamily(String expression) throws Exception
     {
-<<<<<<< HEAD
 	setSurfaceFamily(ParserService.parse(expression), expression);
-=======
-	setSurfaceFamily(AlgebraicExpressionParser.parse(expression), expression);
->>>>>>> ae21e7714c6a385ce00e4031b769abb6e9869b67
     }
 
     private void clearExpressionCache()
