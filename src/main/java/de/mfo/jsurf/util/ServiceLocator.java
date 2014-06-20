45e04ed525a95c407f1a15fc5eb6e31916f6af5e
ferpe <ferpe.email@gmail.com>
2014-06-20 19:48:03 -0300

    Math service for web

--- src/main/java/de/mfo/jsurf/util/ServiceLocator.java
+++ src/main/java/de/mfo/jsurf/util/ServiceLocator.java
@@ -1,4 +1,3 @@
-
 package de.mfo.jsurf.util;
 
 import de.mfo.jsurf.parser.ParserService;
@@ -12,20 +11,6 @@ public class ServiceLocator
 
     public static MathService getMathService()
     {
-	return new MathService()
-	{
-	    public double nextPowerOfTwo(double d) // computes the next power of two with respect to outward rounding
-	    {
-		return Math.pow(2, Math.ceil(Math.log(d) / Math.log(2)));
-
-		//	long bits= java.lang.Double.doubleToLongBits(d);
-		//	if ((bits & 0x000fffffffffffffL) != 0L)
-		//	{
-		//	    bits= java.lang.Double.doubleToLongBits(2.0 * d); // "round up" -> increase exponent by one
-		//	    bits&= 0xfff0000000000000L; // remove mantissa bits
-		//	}
-		//	return java.lang.Double.longBitsToDouble(bits);
-	    }
-	};
+	return new JsMathService();
     }
 }
